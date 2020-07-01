11b605792da85900d8a3a04b52fd72285873fa36
Zigrid <akemii.yuki.neko@gmail.com>
2020-07-01 17:13:39 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/isc/zigrid/basedattossqlitezig/BaseDatos/DatosConexion.java
@@ -0,0 +1,34 @@
+package mx.edu.tesoem.isc.zigrid.basedattossqlitezig.BaseDatos;
+
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+public class DatosConexion {
+    private SQLiteDatabase basedatos;
+    private DatosHelper datosHelper;
+    private Context context;
+
+    public DatosConexion(Context context) {
+        this.context = context;
+    }
+
+    public DatosConexion open() {
+
+        datosHelper = new DatosHelper(context);
+        basedatos = datosHelper.getWritableDatabase();
+        return this;
+    }
+
+    public void close() {
+        datosHelper.close();
+    }
+
+    public boolean insertar(ContentValues contentValues) {
+        boolean estado = true;
+        int restadoconsulta = (int) basedatos.insert(DatosHelper.tabladatos.TABLA, null, contentValues);
+        if (restadoconsulta < 0) estado=false;
+        return estado;
+    }
+}
